b4b21e68621980e549c488b9802d38db18398ff6
Rodrigo O.C. Pereira <68402662+digocorbellini@users.noreply.github.com>
2025-11-06 00:26:37 -0800

    resource manager cache brain dump comment added for future planning

--- code/src/core/resource_management/resource_manager.cpp
+++ code/src/core/resource_management/resource_manager.cpp
@@ -1,5 +1,15 @@
 #include "resource_manager.h"
 
+// TODO: potentially create a static cache of resources which can be added to and removed from
+// as scenes are loaded and unloaded
+// Can be structured as so:
+// - Create a struct which contains: HashedString (used for hashing), pointer to resource
+// - Create a static array of these structs and use this as a linear probing map which you can index
+//   into using HashedStrings
+// - Dynamically allocate resources using heap and assign those pointers to the struct
+//		- Might have to store the resources as void pointers in the struct and then cast them
+//		  to the desired type when they are requested using templated resources 
+
 ResourceManager::ResourceManager()
 {
 
