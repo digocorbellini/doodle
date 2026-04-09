cb38450fb160b169bf23d014107122c9db1a3996
Rodrigo O.C. Pereira <68402662+digocorbellini@users.noreply.github.com>
2026-04-08 23:56:27 -0700

    create system base class

---
+++ code/src/core/system.h
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////
+//
+// Definition of the virtual "System" class which all 
+// systems will inherit from. Systems are meant to be 
+// static classes that automatically register themselves
+// to the ecs system, which gives them access to some 
+// optional methods to hook into game events and loops.
+//
+////////////////////////////////////////////////////////////
+
+#pragma once
+#include "common/types.h"
+#include "core/ecs.h"
+
+class System
+{
+public:
+	System()
+	{
+		ECS_RegisterSystem( this );
+	}
+
+	virtual ~System() = default;
+
+	/// <summary>
+	/// Called once per frame at the start of a frame (before the regular frame logic is run)
+	/// </summary>
+	/// <param name="deltaTime">The time since the last frame start</param>
+	/// <param name="numEntities">The number of entities that currently exist</param>
+	/// <param name="componenents">All componenets that currently exist</param>
+	virtual void OnFrameStart( const double deltaTime, const EntityID numEntities, Components* componenents ) {};
+
+	/// <summary>
+	/// Called once per frame 
+	/// </summary>
+	/// <param name="deltaTime">The time since the last frame start</param>
+	/// <param name="numEntities">The number of entities that currently exist</param>
+	/// <param name="componenents">All componenets that currently exist</param>
+	virtual void OnFrame( const double deltaTime, const EntityID numEntities, Components* componenents ) {};
+
+	/// <summary>
+	/// Called once per frame at the end of a frame (after the regular frame logic is run)
+	/// </summary>
+	/// <param name="deltaTime">The time since the last frame start</param>
+	/// <param name="numEntities">The number of entities that currently exist</param>
+	/// <param name="componenents">All componenets that currently exist</param>
+	virtual void OnFrameEnd( const double deltaTime, const EntityID numEntities, Components* componenents ) {};
+};
