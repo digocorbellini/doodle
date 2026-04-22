590cab3ad39f8d4c99239495a734a2c070bf5b61
Rodrigo O.C. Pereira <68402662+digocorbellini@users.noreply.github.com>
2026-04-21 22:36:35 -0700

    fix compile error

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -7,33 +7,6 @@
 int main ()
 {
     // TODO: have to call all "Init"s for all core systems
-    // then have to start game loop
-    //Resource *test = new TextureResource( "hi there!" );
-    
-    //sf::RenderWindow window ( sf::VideoMode ( 200, 200 ), "SFML works!" );
-    //sf::CircleShape shape ( 100.f );
-    //shape.setFillColor ( sf::Color::Green );
-
-    //// TODO: testing
-    //SceneLoader::GetInstance().LoadSceneFile( "" );
-
-
-    //while( window.isOpen () )
-    //{
-    //    sf::Event event;
-    //    while( window.pollEvent ( event ) )
-    //    {
-    //        if( event.type == sf::Event::Closed )
-    //            window.close ();
-    //    }
-
-    //    window.clear ();
-    //    window.draw ( shape );
-    //    window.display ();
-    //}
-
-    // TODO: remove this testing logic
-    ResourceManager_LoadSceneAssets( nullptr );
 
     ECS_StartGameLoop();
 
