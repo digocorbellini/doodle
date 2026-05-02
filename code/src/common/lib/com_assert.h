92d76de8f563a2633ba29ee83c2e1f4ca7edf5e2
Rodrigo O.C. Pereira <68402662+digocorbellini@users.noreply.github.com>
2026-05-02 00:05:20 -0700

    fix potential issue with assert not having any arguments

--- code/src/common/lib/com_assert.h
+++ code/src/common/lib/com_assert.h
@@ -15,9 +15,9 @@
 #define COM_ASSERT( expression, fmt, ... )		\
 {												\
 	if ( !(expression) )						\
-	{											\
+	{										\
 		fprintf( stderr, "ASSERT: " );			\
-		fprintf( stderr, fmt, __VA_ARGS__ );	\
+		fprintf( stderr, fmt, ##__VA_ARGS__ );	\
 		assert( false );						\
 	}											\
 }
