73697fd03c6a8125ced80a6c7479b91ead757746
Rodrigo O.C. Pereira <68402662+digocorbellini@users.noreply.github.com>
2026-05-08 00:05:58 -0700

    add some basic vector 2 math helpers

---
+++ code/src/common/lib/math/vector_math.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <algorithm>
+
+namespace Vec2Math
+{
+	template<typename T>
+	inline sf::Vector2<T> Scale( const sf::Vector2<T>& a, const sf::Vector2<T>& b )
+	{
+		return  sf::Vector2<T>( a.x * b.x, a.y * b.y );
+	}
+
+	template<typename T>
+	inline sf::Vector2<T> Scale( const sf::Vector2<T>& a, const T scalar )
+	{
+		return  sf::Vector2<T>( a.x * scalar, a.y * scalar );
+	}
+
+	template<typename T>
+	inline T Magnitude( const sf::Vector2<T>& a )
+	{
+		return std::sqrt( ( a.x * a.x ) + ( a.y * a.y ) );
+	}
+
+	template<typename T>
+	inline sf::Vector2<T> Normalize( const sf::Vector2<T>& a )
+	{
+		const T magnitude = Magnitude( a );
+		if ( magnitude == 0 )
+		{
+			return sf::Vector2<T>( 0, 0 );
+		}
+		return sf::Vector2<T>( a.x / magnitude, a.y / magnitude );
+	}
+
+	template<typename T>
+	inline bool Equals( const sf::Vector2<T>& a, const sf::Vector2<T>& b )
+	{
+		return a.x == b.x && a.y == b.y;
+	}
+
+	template<typename T>
+	inline T Distance( const sf::Vector2<T>& a, const sf::Vector2<T>& b )
+	{
+		const T xDist = a.x - b.x;
+		const T yDist = a.y - b.y;
+		return ( xDist * xDist ) + ( yDist * yDist );
+	}
+}
